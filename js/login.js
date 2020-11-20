967ac45920469f7287b05a5f9ba851e01511e782
Yuri Lima Gonçalves <yurilimagoncalves@gmail.com>
2020-11-20 18:05:27 -0300

    Update

--- js/login.js
+++ js/login.js
@@ -1,42 +1,52 @@
 $(document).ready(function(){
+    init();
 
-    $('#btn-entrar').click(function(){
-        let ra = $('#inputUsuario').val();
-        let senha = $('#inputSenha').val();
-        $.ajax({
-            url: "http://www.felipemaciel.com.br/sys/fatec/appFatec/logar.php",
-            data: {
-                ra: ra,
-                senha: senha
-            },
-            success: function(resposta){
-                if(resposta == "Erro!"){
-                    // $('#toast-erro').toast('show');
-                    console.log(resposta);
-                }else{
-                    
-                    // window.open("admin2.html","_self");
-                    // $('#toast-success').toast('show');
-                    console.log(resposta);
-                    // idUsuario = resposta;
-                    // $('#login').animate({
-                    //     left: "-100%"
-                    // });
-                    
-                    // $('#inicial').show().animate({
-                    //     left: "0px" 
-                    // }, function(){
-                    //     carregarLista(idUsuario);
-                    // });
-                    
-                }
-                
-            },
-            error: function(erro){
-                // M.toast({html: erro}); 
-                alert(erro);
-            }
+    function init(){
+        inicializarComponentes();
+        sair();
+        logar();
+    }
+
+    function inicializarComponentes(){
+        
+    }
+
+    function sair(){
+        $('#btn-sair').click(function(){
+            window.open("login2.html","_self");
         });
-    });
+    }
+
+    function logar(){
+        let usuario = null;
+        $('#btn-entrar').click(function(){
+            let ra = $('#inputUsuario').val();
+            let senha = $('#inputSenha').val();
+            $.ajax({
+                url: "http://www.felipemaciel.com.br/sys/fatec/appFatec/logar.php",
+                data: {
+                    ra: ra,
+                    senha: senha
+                },
+                success: function(resposta){
+                    if(resposta == "Erro!"){
+                        $('#toast-erro').toast('show');
+                    }else{
+                        
+                        window.open("admin3.html","_self");
+                        usuario = resposta;
+                        
+                    }
+                    
+                },
+                error: function(erro){
+                    // M.toast({html: erro}); 
+                    alert(erro);
+                }
+            });
+        });
+    }
+
+    
     
 });
