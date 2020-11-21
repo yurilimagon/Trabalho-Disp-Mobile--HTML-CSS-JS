c8c00da8125d8ff5dfd662c4d0bb22a193226745
Yuri Lima Gonçalves <yurilimagoncalves@gmail.com>
2020-11-20 23:29:30 -0300

    UPGRADE index

--- js/index.js
+++ js/index.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
     }
 
     function logar(){
-        $('#tela-login').hide();
+        $('#tela-aluno').hide();
         $('#btn-entrar').click(function(){
             let ra = $('#inputUsuario').val();
             let senha = $('#inputSenha').val();
@@ -36,12 +36,18 @@ $(document).ready(function(){
                     }else{
                         $('#tela-login').hide();
                         $('#tela-aluno').show();
+                        $('#cont-disciplinas').hide();
                         usuario = JSON.parse(resposta);
-                        // window.open("admin2.html","_self");                        
+                        // window.open("admin2.html","_self");   
+                        let idAluno = usuario[0].idAluno;
+                        let ra = usuario[0].ra;
                         let nome = usuario[0].nome;
+                        let email = usuario[0].email;
                         console.log(nome);
                         $('#lb-nome').text(nome);
-                        // perfil();
+                        $('#lb-id').text(ra);
+                        $('#lb-email').text(email);
+                        exibeHome();
                     }
                     
                 },
@@ -53,9 +59,62 @@ $(document).ready(function(){
         });
     }
 
-    function perfil(){
-        let nome = usuario[0].nome;
-        console.log(nome);
-        $('#lb-nome').text("nome");
+    $('#btn-disciplinas').click(function(){
+        $('.nav-link').removeClass('active');
+        $('#btn-disciplinas').addClass('active');
+        exibeDisciplinas(usuario[0].idAluno);
+    });
+
+    $('#btn-home').click(function(){
+        $('.nav-link').removeClass('active');
+        $('#btn-home').addClass('active');
+        exibeHome();
+    });
+
+    function exibeDisciplinas(idAluno){
+        var conteudo;
+        var dados = [];
+        $.getJSON('http://www.felipemaciel.com.br/sys/fatec/appFatec/getDisciplinas.php?idAluno='+idAluno, function(data){
+            $.each(data, function(chave, valor){
+
+                dados += `<div class="card">
+                            <div class="card-header" id="${valor.idDisciplina}">
+                            <h2 class="mb-0">
+                                <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" 
+                                        data-target="#collapse${valor.idDisciplina}" aria-expanded="true" aria-controls="${valor.idDisciplina}">
+                                ${valor.disciplina}
+                                </button>
+                            </h2>
+                            </div>
+                        
+                            <div id="collapse${valor.idDisciplina}" class="collapse hide" aria-labelledby="${valor.idDisciplina}" 
+                                 data-parent="#accordionExample">
+                                <div class="card-body text-left">
+                                    <p>Presença: ${valor.presenca} </p>
+                                    <p>Faltas: ${valor.falta}</p>
+                                    <p>NotaP1: ${valor.notaP1}</p>
+                                    <p>NotaP2: ${valor.notaP2}</p>
+                                    <p>NotaT1: ${valor.notaT1}</p>
+                                    <p>NotaT2: ${valor.notaT2}</p>
+                                </div>
+                            </div>
+                        </div>`
+                
+            });
+            conteudo = `
+                        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+                            <h1 class="h2">Disciplinas</h1>
+                        </div >
+                        <div class="accordion" id="accordionExample">${dados}</div>`
+            $('#conteudo').html(conteudo);
+        });
     }
+
+    function exibeHome(){
+        conteudo = `<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+                        <h1 class="h2">Bem Vindo, Aluno!</h1>
+                    </div >`
+        $('#conteudo').html(conteudo);
+    }
+    
 });
